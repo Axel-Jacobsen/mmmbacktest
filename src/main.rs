9a27247081895704c323efb6e4526e4fcf62c26a
Axel Jacobsen <axelnj44@gmail.com>
2023-12-31 17:18:47 -0800

    ok got my footing!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use warp::http::StatusCode;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let root = warp::path::end().map(|| StatusCode::NOT_IMPLEMENTED);
+    let base = warp::path("v0").map(|| StatusCode::NOT_IMPLEMENTED);
+
+    let routes = root.or(base);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }

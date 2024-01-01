50f90e1747bc50fdea8c013675d9d67428067305
Axel Jacobsen <axelnj44@gmail.com>
2023-12-31 17:21:15 -0800

    wee

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     let root = warp::path::end().map(|| StatusCode::NOT_IMPLEMENTED);
-    let base = warp::path("v0").map(|| StatusCode::NOT_IMPLEMENTED);
+    let base = warp::path("v0")
+        .and(warp::path::end())
+        .map(|| StatusCode::NOT_IMPLEMENTED);
 
     let routes = root.or(base);
 

40d9aecb18fab3ef9c38d3d21747ad3c7d3cd800
Axel Jacobsen <axelnj44@gmail.com>
2024-01-04 17:55:46 -0800

    too annoying!

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,13 @@
-use rusqlite::{params, Connection, Result};
+use diesel::{prelude::QueryResult, sqlite::SqliteConnection, Connection, RunQueryDsl};
 
-use crate::data_types as dt;
+
+use serde_json::Value;
+
+use std::collections::HashMap;
+use std::fs;
+
+use crate::data_types::{Bet, LiteMarket, FullMarket};
+use crate::schema::bets::dsl::*;
 
 // https://chat.openai.com/share/d2e2e7a3-80ed-4502-b090-9eb6237c5c74
 // Geez this is harder than I thought. I hope that throwing it all into a sqlite db
@@ -11,3 +18,60 @@ use crate::data_types as dt;
 //
 // Actually, there *has* to be a better way. This is insane. Maybe just making my own index is
 // fine.
+
+pub fn establish_connection() -> SqliteConnection {
+    SqliteConnection::establish("mmmbacktest.sqlite3")
+        .unwrap_or_else(|_| panic!("Error connecting to database"))
+}
+
+fn check_table_exists(conn: &mut SqliteConnection, table_name: &str) -> bool {
+    let query = format!("SELECT 1 FROM {} LIMIT 1", table_name);
+    diesel::sql_query(query).execute(conn).is_ok()
+}
+
+
+fn create_tables_if_not_exists(conn: &mut SqliteConnection) -> QueryResult<usize> {
+    diesel::sql_query(
+        "CREATE TABLE bets (
+            id TEXT PRIMARY KEY,
+            user_id TEXT NOT NULL,
+            user_avatar_url TEXT,
+            user_name TEXT,
+            user_username TEXT,
+            contract_id TEXT NOT NULL,
+            answer_id TEXT,
+            created_time BIGINT NOT NULL,
+            amount FLOAT NOT NULL,
+            loan_amount FLOAT,
+            outcome TEXT NOT NULL,
+            shares FLOAT NOT NULL,
+            prob_before FLOAT NOT NULL,
+            prob_after FLOAT NOT NULL,
+            is_api BOOL,
+            is_ante BOOL NOT NULL,
+            is_redemption BOOL NOT NULL,
+            is_challenge BOOL NOT NULL,
+            challenge_slug TEXT,
+            reply_to_comment_id TEXT,
+            fees TEXT,
+            limit_props TEXT,
+            shares_by_outcome TEXT
+        )"
+    ).execute(conn)
+}
+
+
+pub fn setup_db() -> SqliteConnection {
+    let mut conn = establish_connection();
+    if !check_table_exists(&mut conn, "bets") {
+        create_tables_if_not_exists(&mut conn);
+    }
+    conn
+}
+
+fn insert_bets(conn: &SqliteConnection, betss: Vec<Bet>) {
+    diesel::insert_into(bets::table)
+        .values(&betss)
+        .execute(conn)
+        .expect("Error inserting Bet")
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 use warp::http::StatusCode;
 use warp::Filter;
 
+mod schema;
 mod data_types;
 mod db;
 
@@ -15,17 +16,15 @@ fn iter_over_markets(market_json: &String) -> Vec<data_types::FullMarket> {
 fn iterate_over_bets(bets_dir: &String) -> Vec<data_types::Bet> {
     let mut bets: Vec<data_types::Bet> = vec![];
     for file in fs::read_dir(bets_dir).unwrap() {
-        println!("{:?}", file);
         let path = file.unwrap().path();
 
         if path.extension().and_then(|s| s.to_str()) != Some("json") {
-            println!("continuing with {:?}", path);
             continue;
         }
 
         let file_as_string = fs::read_to_string(path).unwrap();
-        let inner_bets: Vec<data_types::Bet> = serde_json::from_str(&file_as_string).unwrap();
-        println!("{:?}", inner_bets.len());
+        let inner_bets: Vec<data_types::Bet> =
+            serde_json::from_str(&file_as_string).expect("failed to parse json into Bet struct");
         bets.extend(inner_bets);
     }
     bets
@@ -33,18 +32,18 @@ fn iterate_over_bets(bets_dir: &String) -> Vec<data_types::Bet> {
 
 #[tokio::main]
 async fn main() {
+    let conn = db::setup_db();
+
+    return;
+
     let markets =
         iter_over_markets(&"backtest-data/manifold-dump-markets-04082023.json".to_string());
 
-    for market in markets {
-        println!("{:?}", market);
-    }
+    println!("found {:?} markets", markets.len());
 
     let bets = iterate_over_bets(&"backtest-data/bets".to_string());
 
-    for bet in bets {
-        println!("{:?}", bet);
-    }
+    println!("found {:?} bets", bets.len());
 
     let root = warp::path::end().map(|| StatusCode::NOT_IMPLEMENTED);
     let base = warp::path("v0")

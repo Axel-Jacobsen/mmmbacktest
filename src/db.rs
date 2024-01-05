262a502ea28f77ed93f6ebe4a710e01fde66204a
Axel Jacobsen <axelnj44@gmail.com>
2024-01-04 21:31:22 -0800

    rm comment
    making your own index and query engine is hard

--- src/db.rs
+++ src/db.rs
@@ -5,16 +5,6 @@ use std::fs;
 
 use crate::data_types::{Bet, FullMarket, LiteMarket};
 
-// https://chat.openai.com/share/d2e2e7a3-80ed-4502-b090-9eb6237c5c74
-// Geez this is harder than I thought. I hope that throwing it all into a sqlite db
-// won't be too painful. I wish I knew more about this. Enough for today. Why is sql
-// serialization so ugly?? I hate that I have to go convert json to a rust struct to
-// convert it to a sqlite struct, just to be converted back to a rust struct and then
-// serialized back into json. That's awful.
-//
-// Actually, there *has* to be a better way. This is insane. Maybe just making my own index is
-// fine.
-
 fn iter_over_markets(market_json: &String) -> Vec<FullMarket> {
     let file_as_string = fs::read_to_string(market_json).unwrap();
     let markets: Vec<FullMarket> = serde_json::from_str(&file_as_string).unwrap();

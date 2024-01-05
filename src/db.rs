6394f07d712238bb72c1e27e81dbb57b068f05fb
Axel Jacobsen <axelnj44@gmail.com>
2024-01-04 20:58:50 -0800

    fix last sql errors!

--- src/db.rs
+++ src/db.rs
@@ -20,25 +20,6 @@ fn iter_over_markets(market_json: &String) -> Vec<FullMarket> {
     markets
 }
 
-// fn iterate_over_bets(bets_dir: &String) -> Vec<Bet> {
-//     let mut bets: Vec<Bet> = vec![];
-
-//     for file in fs::read_dir(bets_dir).unwrap() {
-//         let path = file.unwrap().path();
-
-//         if path.extension().and_then(|s| s.to_str()) != Some("json") {
-//             continue;
-//         }
-
-//         let file_as_string = fs::read_to_string(path).unwrap();
-//         let inner_bets: Vec<Bet> =
-//             serde_json::from_str(&file_as_string).expect("failed to parse json into Bet struct");
-//         bets.extend(inner_bets);
-//     }
-
-//     bets
-// }
-
 fn iterate_over_bets(bets_dir: &str) -> impl Iterator<Item = Vec<Bet>> {
     let paths = fs::read_dir(bets_dir).unwrap_or_else(|err| {
         panic!("Error reading directory: {}", err);
@@ -141,7 +122,7 @@ pub fn bulk_insert_bets(conn: &mut Connection, bets: &Vec<Bet>) -> Result<usize>
             ?1, ?2, ?3, ?4, ?5, ?6, ?7,
             ?8, ?9, ?10, ?11, ?12, ?13,
             ?14, ?15, ?16, ?17, ?18, ?19, ?20,
-            ?21, ?22, ?23, ?24
+            ?21, ?22, ?23
         )";
 
     for chunk in bets.chunks(1000) {
--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ mod db;
 async fn main() {
     let mut conn = db::setup_db();
 
-    // let root = warp::path::end().map(|| StatusCode::NOT_IMPLEMENTED);
-    // let base = warp::path("v0")
-    //     .and(warp::path::end())
-    //     .map(|| StatusCode::NOT_IMPLEMENTED);
+    let root = warp::path::end().map(|| StatusCode::NOT_IMPLEMENTED);
+    let base = warp::path("v0")
+        .and(warp::path::end())
+        .map(|| StatusCode::NOT_IMPLEMENTED);
 
-    // let routes = root.or(base);
+    let routes = root.or(base);
 
     // warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }

5c4d0ba762d248f745ee0bb2af72cd12e72e8d67
Axel Jacobsen <axelnj44@gmail.com>
2024-01-04 23:33:34 -0800

    not pub

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,12 +1,14 @@
-pub mod bet_table;
+mod bet_table;
 mod db_common;
-pub mod market_table;
+mod market_table;
 
 use rusqlite::Connection;
 
 use crate::db::bet_table::init_bet_table;
 use crate::db::db_common::get_db_connection;
 use crate::db::market_table::init_market_table;
+
+
 
 pub fn setup_db() -> Connection {
     let mut conn = get_db_connection().expect("failed to get db connection");

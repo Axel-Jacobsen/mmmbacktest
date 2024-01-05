4f1bba266db8a6803b6dcd53cb5ee9c6334bff7c
Axel Jacobsen <axelnj44@gmail.com>
2024-01-04 23:33:49 -0800

    fx

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -8,8 +8,6 @@ use crate::db::bet_table::init_bet_table;
 use crate::db::db_common::get_db_connection;
 use crate::db::market_table::init_market_table;
 
-
-
 pub fn setup_db() -> Connection {
     let mut conn = get_db_connection().expect("failed to get db connection");
 
